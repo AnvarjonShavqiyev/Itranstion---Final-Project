699dd05d68ea90f5526fc8566601b86ee73865b4
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-03-18 11:26:13 +0500

    view collection&item added

--- client/src/components/item/Item.tsx
+++ client/src/components/item/Item.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Item } from "../../types/ElementTypes"
 import { useTranslation } from "react-i18next"
 const ItemC = ({item}:{item:Item}) => {
@@ -6,6 +7,9 @@ const ItemC = ({item}:{item:Item}) => {
     <div className="collection-wrapper" key={item._id}>
       <img width={400} height={350} src={item.image} alt="" />
       <div className="collection-info">
+        <Link to={`/singleItem/${item._id}`}>View item</Link>
+      </div>
+      <div className="collection-info-wrapper">
         <p>{item.name}</p>
         <p>
           {item.comments.length} {item.comments.length > 1 ? ' ' + t('comments') : ' ' + t('comment')}
